a4ca9b5c90a41cb8419f41aa0ba3405d8d31ec0e
jonaszeferino <jonaszeferino@gmail.com>
2023-12-08 14:52:19 -0300

    new link on footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,15 +7,31 @@ export default function Footer() {
     <footer className={styles.footer}>
       <ChakraProvider>
         <VStack align="start">
-          <Box ml="40px">Home</Box>
-          <Box ml="40px">What to Watch Today?</Box>
-          <Box ml="40px">Where Is My movie?</Box>
+          <Link href="/">
+            <Box ml="40px">Home</Box>
+          </Link>
+          <Link href="/watch-today">
+            <Box ml="40px">What to Watch Today?</Box>
+          </Link>
+          <Link href="/where-is-my-movie">
+            <Box ml="40px">Where Is My movie?</Box>
+          </Link>
           <Box ml="40px">&copy;Jonas Zeferino 2023</Box>
         </VStack>
+      
         <VStack ml="40px" align="start">
-          <Box ml="40px">Login</Box>
-          <Box ml="40px">Tv Shows</Box>
-          <Box ml="40px">Movies</Box>
+          <Link href="/signUp">
+            <Box ml="40px">Login</Box>
+          </Link>
+
+          <Link href="/search-tvshows">
+            <Box ml="40px">Tv Shows</Box>
+          </Link>
+
+          <Link href="/search-movies">
+            <Box ml="40px">Movies</Box>
+          </Link>
+
           <Box ml="40px" display="flex" alignItems="center" mr="10px">
             <Text marginRight="4px">Powered by</Text>
             <Link
